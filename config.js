aa9f0493379753b518c751b13c2d47d0c695e2dd
masonp-op <masonpluard24@gmail.com>
2026-03-05 16:20:50 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Change these to customize the puzzle
+const GRID_SIZE = 4;
+const PUZZLE_IMAGE = 'https://picsum.photos/400/400';
+const REVEAL_MESSAGE = 'You did it! This is your secret message.';

c7f641136868179bac7a11491f992058567a9e02
masonp-op <masonpluard24@gmail.com>
2026-03-06 13:22:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Change these to customize the puzzle
 const GRID_SIZE = 3;
 const PUZZLE_IMAGE = 'https://picsum.photos/400/400';
-const REVEAL_MESSAGE = 'I think we should stp seeing eachother!';
+const REVEAL_MESSAGE = 'I think we should stop seeing eachother!';

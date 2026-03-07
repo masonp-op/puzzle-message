a0ca36f1dc8afe29abd50a03dc6c82e4d2a171e8
masonp-op <masonpluard24@gmail.com>
2026-03-07 03:58:39 -0600

    Change GRID_SIZE from 3 to 6

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Change these to customize the puzzle
-const GRID_SIZE = 3;
+const GRID_SIZE = 6;
 const PUZZLE_IMAGE = 'https://picsum.photos/400/400';
 const REVEAL_MESSAGE = 'I think we should stop seeing eachother!';
